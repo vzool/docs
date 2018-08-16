faef18785131d1d34220b8aff56c47fd596f3ab6
rijkvanzanten <rijkvanzanten@me.com>
2018-08-16 17:00:38 -0400

    Update homepage config and rename reference

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,13 +1,22 @@
 module.exports = {
-  base: '/docs/',
+  base: '/',
   title: 'Directus Docs',
   description: 'A headless CMS that manages your content, not your workflow.',
   themeConfig: {
     nav: [
-      { text: 'Home', link: 'https://docs.directus.io' },
-      { text: 'API Reference', link: '/api' }
+      { text: 'Home', link: '/' },
+      { text: 'App', items: [
+        { text: 'User Guide', link: '/app/user-guide/' },
+        { text: 'Admin Guide', link: '/app/admin-guide/' },
+        { text: 'Contributor Guide', link: '/app/contributor-guide/' }
+      ]},
+      { text: 'API', items: [
+        { text: 'API Reference', link: '/api/reference' },
+        { text: 'Admin Guide', link: '/api/admin-guide/' },
+        { text: 'Contributor Guide', link: '/api/contributor-guide/' }
+      ]}
     ],
-    repo: 'directus/docs/v7',
+    repo: 'directus/docs-v7',
     docsDir: 'docs',
     editLinks: true,
   }
