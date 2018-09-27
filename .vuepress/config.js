e3ad1c1ccc5fe1c9b5524cc91b1ccc31a00e86b2
Ben Haynes <ben@rngr.org>
2018-09-27 16:18:27 -0400

    Favicons

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,19 @@ module.exports = {
   base: '/',
   title: 'Directus Docs',
   description: 'The All-New Directus 7. Future-Proof Headless CMS for Managing Custom Database Content.',
+  head: [
+    ['link', { rel: "manifest",  href: "/site.webmanifest" }],
+    ['link', { rel: "mask-icon",  href: "/safari-pinned-tab.svg", color: "#5bbad5" }],
+    ['link', { rel: "shortcut icon", type: "image/x-icon", href: "/favicon.ico" }],
+    ['link', { rel: "apple-touch-icon", type: "image/x-icon", sizes: "180x180", href: "/apple-touch-icon.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" }],
+    ['meta', { name: "application-name", content: "Directus Docs" }],
+    ['meta', { name: "theme-color", content: "#263238" }],
+    ['meta', { name: "apple-mobile-web-app-title", content: "Directus Docs" }],
+    ['meta', { name: "msapplication-TileColor", content: "#263238" }],
+    ['meta', { name: "msapplication-config", content: "/browserconfig.xml" }]
+  ],
   themeConfig: {
     sidebar: 'auto',
     lastUpdated: 'Last Updated',
